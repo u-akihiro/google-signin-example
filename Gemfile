b2594f51db823873475182a687b1e90ab588c020
Akihiro Uesaka <akihiro.uesaka@gmail.com>
2020-03-16 00:18:47 +0900

    ID Tokenの検証を追加

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'https://rubygems.org'
 gem 'sinatra'
+gem 'google-id-token'
--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra'
+require 'google-id-token'
+require 'json'
 
 get '/' do
   erb :index
 end
 
-get '/authenticated' do
-    erb :authn
+post '/verify' do
+    content_type :json
+
+    token = params[:token]
+    validator = GoogleIDToken::Validator.new
+    begin
+      payload = validator.check(token, ENV['CLIENT_ID'])
+      payload.to_json
+    rescue GoogleIDToken::ValidationError => e
+      report "Cannot validate: #{e}"
+    end
 end
