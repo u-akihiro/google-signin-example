24cb4ca12f2e1146d69976626c5e011b2f971740
Akihiro Uesaka <akihiro.uesaka@gmail.com>
2020-03-16 00:43:06 +0900

    レスポンス修正

--- app.rb
+++ app.rb
@@ -15,6 +15,6 @@ post '/verify' do
       payload = validator.check(token, ENV['CLIENT_ID'])
       payload.to_json
     rescue GoogleIDToken::ValidationError => e
-      "Cannot validate: #{e}"
+      {message: "Cannot validate: #{e}"}.to_json
     end
 end
